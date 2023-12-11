15d897f9ce095ca86b2f19aebc6ba374c6f4eb78
abdullahthabit <31736084+abdullahthabit@users.noreply.github.com>
2023-12-11 15:32:02 +0100

    add coverage support and updated min test function

--- tests/test_models.py
+++ tests/test_models.py
@@ -30,18 +30,18 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
-def test_daily_max_negatives():
+def test_daily_min_negatives():
     """Test that max function works for an array of negative integers."""
-    from inflammation.models import daily_max
+    from inflammation.models import daily_min
 
     test_input = np.array([[-1, -2],
                            [-3, -4],
                            [-5, -6]])
 
-    test_result = np.array([-1, -2])
+    test_result = np.array([-5, -6])
 
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
+    npt.assert_array_equal(daily_min(test_input), test_result)
 
 def test_daily_min_string():
     """Test for type error when passing strings"""
